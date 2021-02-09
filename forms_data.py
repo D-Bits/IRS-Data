83536098e103f3f3f4906bc6b77df3164541d326
D-Bits <lockwood.da88@gmail.com>
2021-02-09 12:10:53 -0800

    Misc changes.

--- forms_data.py
+++ forms_data.py
@@ -4,6 +4,6 @@ import pandas as pd
 url = "https://apps.irs.gov/app/picklist/list/priorFormPublication.html?indexOfFirstRow=0&sortColumn=sortOrder&value=&criteria=&resultsPerPage=200&isDescending=false"
 
 data = pd.read_html(url)
-df = pd.DataFrame(data, columns=['Index', 'Product Number', 'Title', 'Revision Date'])
+df = pd.DataFrame(data)
 json_data = df.to_json("./dumps/irs_forms.json", orient="records")
 print(df)
