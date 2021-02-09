e2f3808d90bd48249d5275ad600232fe4962e804
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2021-02-09 11:00:43 -0800

    More tweeking of forms scraper.

--- forms_data.py
+++ forms_data.py
@@ -4,5 +4,6 @@ import pandas as pd
 url = "https://apps.irs.gov/app/picklist/list/priorFormPublication.html?indexOfFirstRow=0&sortColumn=sortOrder&value=&criteria=&resultsPerPage=200&isDescending=false"
 
 data = pd.read_html(url)
-df = pd.DataFrame(data)
+df = pd.DataFrame(data, columns=['Index', 'Product Number', 'Title', 'Revision Date'])
 json_data = df.to_json("./dumps/irs_forms.json", orient="records")
+print(df)
