6542cfa2af1cbbae5d056a7d16805760f778b962
D-Bits <lockwood.da88@gmail.com>
2021-02-10 12:58:45 -0800

    More work on forms_download

--- forms_download.py
+++ forms_download.py
@@ -3,12 +3,22 @@ import requests
 import re
 
 
-req = requests.get("https://apps.irs.gov/app/picklist/list/priorFormPublication.html?indexOfFirstRow=0&sortColumn=sortOrder&value=&criteria=&resultsPerPage=200&isDescending=false")
-# Throw an error if website is inaccessible 
-if req.status_code != 200:
-    print("ERROR:", req.status_code)
+def download_files(url):
 
-soup = BeautifulSoup(req.text, "lxml")
-# Scrape links to downloads
-links = soup.find_all('a', class="LeftCellSpacer")
+    req = requests.get(url, stream=True)
+    # Throw an error if website is inaccessible 
+    if req.status_code != 200:
+        print("ERROR:", req.status_code)
 
+    soup = BeautifulSoup(req.text, "lxml")
+    # Scrape links to downloads
+    #links = soup.find_all('td', attrs={'class': "LeftCellSpacer"})
+    #print(links)
+    for a in soup.select('.LeftCellSpacer td a'):
+        res = requests.get(a, stream=True)
+        with open(f"./pdfs/{a}.pdf") as f:
+            f.write(res.content)
+    #downloads = requests.get(links)
+
+
+download_files("https://apps.irs.gov/app/picklist/list/priorFormPublication.html?indexOfFirstRow=0&sortColumn=sortOrder&value=&criteria=&resultsPerPage=200&isDescending=false")
