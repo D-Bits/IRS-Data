0347292bb38934ade930aa08e2ab4033628b9c3f
D-Bits <lockwood.da88@gmail.com>
2021-02-10 12:58:54 -0800

    Misc changes

--- forms_data.py
+++ forms_data.py
@@ -4,13 +4,19 @@ Script for scraping IRS form meta data.
 import pandas as pd
 
 
-try:
-    url = "https://apps.irs.gov/app/picklist/list/priorFormPublication.html?indexOfFirstRow=0&sortColumn=sortOrder&value=&criteria=&resultsPerPage=200&isDescending=false"
-    # Load the HTML from the URL into Pandas
-    data = pd.read_html(url)
-    # Convert the above list variable to a DataFrame, so it can be dumped to JSON.
-    df = pd.DataFrame(data)
-    # Dump the JSON a file in the "dumps" directory. 
-    json_data = df.to_json("./dumps/irs_forms.json", orient="records")
-except:
-    print("An error has occured. We apologize for the inconvience.")
+def get_metadata(url):
+
+    try:
+        # Load the HTML from the URL into Pandas
+        data = pd.read_html(url)
+        # Convert the above list variable to a DataFrame, so it can be dumped to JSON.
+        df = pd.DataFrame(data)
+        # Dump the JSON a file in the "dumps" directory. 
+        df.to_json("./dumps/irs_forms.json", orient="records")
+    except:
+        print("An error has occured. We apologize for the inconvience.")
+
+
+get_metadata("https://apps.irs.gov/app/picklist/list/priorFormPublication.html?indexOfFirstRow=0&sortColumn=sortOrder&value=&criteria=&resultsPerPage=200&isDescending=false")
+
+    
