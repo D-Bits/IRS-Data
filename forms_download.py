449b84d9c77b64ff8e2c418a429954c773336f1f
D-Bits <lockwood.da88@gmail.com>
2021-02-10 23:30:57 -0800

    Got basic functionality working.

--- forms_download.py
+++ forms_download.py
@@ -1,37 +1,35 @@
 from bs4 import BeautifulSoup
 import requests
-import re
+from time import time
 
 
 def get_links(url):
-
     session = requests.session()
     session.proxies = {}
     user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
     session.headers = {'user-agent': user_agent}
     req = session.get(url, stream=False)
-    
-    print(f'{req.text}')
-    # Throw an error if website is inaccessible 
+    # Throw an error if website is inaccessible
     if req.status_code != 200:
         print("ERROR:", req.status_code)
-
-    return req
-
-
+    soup = BeautifulSoup(req.content, features="lxml")
+    links = [l['href'] for l in soup.select('td.LeftCellSpacer a[href]')]
+    # with open('to_download.csv', 'a+') as to_download:
+    #     writer = csv.writer(to_download)
+    #     for l in links:
+    #         writer.writerow(l)
+    return links
 def download_pdfs(links):
-
-    soup = BeautifulSoup(links.text, "lxml")
-    # Scrape links to downloads
-    links = soup.find('td', attrs={'class': "LeftCellSpacer"})
-    children = links.findChildren("a", recursive=False)
-    #print(links)
-    for a in children:
-        res = requests.get(a, stream=True)
-        print(res.status_code)
-        with open(f"./pdfs/{a}.pdf") as f:
-            f.write(res.content)
-    #downloads = requests.get(links)
+    for url in links:
+        try:
+            r = requests.get(url, allow_redirects=True)
+            open(f'{url.split("/")[-1]}', 'wb').write(r.content)
+            time.sleep(1) #take it easy on the IRS
+        except:
+            print(f'failure on {url}')
+def follow_nextpage_link():
+    # get the next page of links and add them to the list
+    pass
 
 
 download_pdfs(get_links("https://apps.irs.gov/app/picklist/list/priorFormPublication.html?indexOfFirstRow=0&sortColumn=sortOrder&value=&criteria=&resultsPerPage=200&isDescending=false"))
