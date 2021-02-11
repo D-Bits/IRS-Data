62bd27aa1b8298684304895dea8fcf741e1e8a24
D-Bits <lockwood.da88@gmail.com>
2021-02-11 15:00:32 -0800

    Finalized forms download.

--- forms_download.py
+++ forms_download.py
@@ -5,16 +5,19 @@ from time import time
 
 # Fetch all the URLs from the website to download PDFs, within a range of years
 def get_links(year_range: range, url:str):
+    # Create a session for getting data 
     session = requests.session()
     session.proxies = {}
     user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
     session.headers = {'user-agent': user_agent}
+    # scarpe the website
     req = session.get(url, stream=False)
     # Throw an error if website is inaccessible
     if req.status_code != 200:
         print("ERROR:", req.status_code)
     soup = BeautifulSoup(req.content, features="lxml")
     links = []
+    # Get the values from the "Revision Date" field, strip out the markup, and append to a list
     for row in soup.select('tr.even'):
         year = int(row.select('td.EndCellSpacer')[0].text.strip())
         link = row.select('td.LeftCellSpacer a[href]')[0]['href']
@@ -22,7 +25,8 @@ def get_links(year_range: range, url:str):
             links.append(link)
             return links
 
-    
+
+# Download the files, using the values returned by the above function    
 def download_pdfs(links: list):
 
     # Get dates from the "Revision Date" field on website
